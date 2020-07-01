554c2f5e28ea52f39a79e6f66ec7ba7478dbc2ef
harsh verma <harshv9988@gmail.com>
2020-07-01 12:15:04 +0530

    added color change for input form on inputting data focus:within

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,23 @@
-$('#task').keypress(function () { 
-    if($(this).val() ==="") {
-        $(this).parents('#description').css('backgroundColor','#343a40');}
-    else {
-        $(this).parents('#description').css('backgroundColor','#51565c');
-    }
-});
+// $('#task').keypress(function () { 
+//     if($(this).val() ==="") {
+//         $(this).parents('#description').css('backgroundColor','#343a40');}
+//     else {
+//         $(this).parents('#description').css('backgroundColor','#51565c');
+//     }
+// });
 
-$('#task-category').onclick(function () { 
-    if($(this).val() ==="") {
-        $(this).parents('#category').css('backgroundColor','#343a40');}
-    else {
-        $(this).parents('#category').css('backgroundColor','#51565c');
-    }
-});
+// $('#task-category').click(function () { 
+//     if($(this).val() ==="") {
+//         $(this).parents('#category').css('backgroundColor','#343a40');}
+//     else {
+//         $(this).parents('#category').css('backgroundColor','#51565c');
+//     }
+// });
+
+// $('#task-date').click(function () { 
+//     if($(this).val() ==="") {
+//         $(this).parents('#description').css('backgroundColor','#343a40');}
+//     else {
+//         $(this).parents('#description').css('backgroundColor','#51565c');
+//     }
+// });
